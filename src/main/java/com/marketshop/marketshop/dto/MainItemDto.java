13b03b7c6c05f9a49049062f60e3188b07907ee0
jeongbeomgyu <jeongbeomgyu@github.com>
2024-08-19 19:05:54 +0900

    메인 화면 아이템 정보

---
+++ src/main/java/com/marketshop/marketshop/dto/MainItemDto.java
@@ -0,0 +1,36 @@
+package com.marketshop.marketshop.dto;
+
+import com.marketshop.marketshop.constant.ItemSellStatus;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MainItemDto {
+
+    private Long id;
+
+    private String itemNm;
+
+    private String itemCategory;
+
+    private ItemSellStatus itemSellStatus;
+
+    private String itemDetail;
+
+    private String imgUrl;
+
+    private Integer price;
+
+    @QueryProjection
+    // 생성자에 @QueryProjection 어노테이션을 선언하여 Querydsl로 결과 조회 시 MainItemDto 객체로 바로 받아옴
+    public MainItemDto(Long id, String itemNm, String itemCategory, String itemDetail, String imgUrl, Integer price) {
+        this.id = id;
+        this.itemNm = itemNm;
+        this.itemCategory = itemCategory;
+        this.itemDetail = itemDetail;
+        this.imgUrl = imgUrl;
+        this.price = price;
+    }
+}
