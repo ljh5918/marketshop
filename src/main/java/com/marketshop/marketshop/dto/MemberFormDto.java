f6941c0a5dad978da84f278ff927d6da028a2cce
jeongbeomgyu <jeongbeomgyu@github.com>
2024-08-19 18:16:58 +0900

    회원 가입 정보 DTO

---
+++ src/main/java/com/marketshop/marketshop/dto/MemberFormDto.java
@@ -0,0 +1,35 @@
+package com.marketshop.marketshop.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+public class MemberFormDto {
+
+    // 회원 가입 화면으로부터 넘어오는 가입정보를 담은 Dto
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String name;
+
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요")
+    private String email;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+    private String password;
+
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
+    private String zipcode;
+
+    private String streetadr;
+
+    private String detailadr;
+
+    private String role;
+
+}
