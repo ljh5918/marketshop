e18af4696f0cbbc3432dca27684b03e5379c8bd9
jeongbeomgyu <jeongbeomgyu@github.com>
2024-08-19 18:47:02 +0900

    주문 상품 정보 DTO

---
+++ src/main/java/com/marketshop/marketshop/dto/OrderItemDto.java
@@ -0,0 +1,25 @@
+package com.marketshop.marketshop.dto;
+
+import com.marketshop.marketshop.entity.OrderItem;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class OrderItemDto {
+
+    private String itemNm;
+
+    private int count;
+
+    private int orderPrice;
+
+    private String imgUrl;
+
+    public OrderItemDto(OrderItem orderItem, String imgUrl) {
+        this.itemNm = orderItem.getItem().getItemNm();
+        this.count = orderItem.getCount();
+        this.orderPrice = orderItem.getOrderPrice();
+        this.imgUrl = imgUrl;
+    }
+}
