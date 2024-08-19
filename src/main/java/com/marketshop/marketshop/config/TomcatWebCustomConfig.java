32c091fdac3c9feaa6d22758b4bf251f0c5cff87
jeongbeomgyu <jeongbeomgyu@github.com>
2024-08-19 19:14:40 +0900

    Tomcat custom config 생성

---
+++ src/main/java/com/marketshop/marketshop/config/TomcatWebCustomConfig.java
@@ -0,0 +1,11 @@
+package com.marketshop.marketshop.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+public class TomcatWebCustomConfig  implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}"));
+    }
+}
