32802632c6b912144eb4ddf65e1e9c3440492381
jeongbeomgyu <jeongbeomgyu@github.com>
2024-10-04 12:12:28 +0900

    React 와 연동하기 위한 CORS 설정

--- src/main/java/com/marketshop/marketshop/config/WebMvcConfig.java
+++ src/main/java/com/marketshop/marketshop/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.marketshop.marketshop.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations(uploadPath);
+    }
+
+    // react 와 연동하기 위한 CORS 설정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // React 애플리케이션의 URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
