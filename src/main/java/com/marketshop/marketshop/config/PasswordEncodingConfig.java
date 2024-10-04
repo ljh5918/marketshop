0f922ca1d9f74fc7dc413e171a09905258b19a0a
jeongbeomgyu <jeongbeomgyu@github.com>
2024-10-04 14:52:49 +0900

    Password Encording

--- src/main/java/com/marketshop/marketshop/config/PasswordEncodingConfig.java
+++ src/main/java/com/marketshop/marketshop/config/PasswordEncodingConfig.java
@@ -3,8 +3,10 @@ package com.marketshop.marketshop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@Component
 public class PasswordEncodingConfig {
 
     @Bean
